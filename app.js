47ff705e3f3d392b3c6394fc6d505a3422be64a8
Your Name <you@example.com>
2022-10-13 22:39:45 +0300

    finished

--- app.js
+++ app.js
@@ -2,33 +2,41 @@ const carouselBox = document.querySelector(".carousel-box");
 const carouselImages = document.querySelectorAll(".carousel-box img");
 
 //buttons
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
 
 //counter
 
 let counter = 1;
 const size = carouselImages[0].clientWidth;
+console.log(size);
 carouselBox.style.transform = "translateX(" + -size * counter + "px)";
 
 //button listeners
 
 nextBtn.addEventListener("click", () => {
+  if (counter >= carouselImages.length - 1) return;
   carouselBox.style.transition = "transform 0.4s ease-in-out";
   counter++;
   carouselBox.style.transform = "translateX(" + -size * counter + "px)";
 });
 
 prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
   carouselBox.style.transition = "transform 0.4s ease-in-out";
   counter--;
   carouselBox.style.transform = "translateX(" + -size * counter + "px)";
 });
-// !!!
+
 carouselBox.addEventListener("transitionend", () => {
   if (carouselImages[counter].id === "lastClone") {
     carouselBox.style.transition = "none";
-    counter = carouselBox.lenght - 2;
+    counter = carouselImages.length - 2;
+    carouselBox.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (carouselImages[counter].id === "firstClone") {
+    carouselBox.style.transition = "none";
+    counter = carouselImages.length - counter;
+    carouselBox.style.transform = "translateX(" + -size * counter + "px)";
   }
 });
-//!!!
